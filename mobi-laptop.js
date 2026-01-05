4ae9c2a58ef19d52c9349d34f3e3e55e04505c7c
karan <karanjarmy@gmail.com>
2026-01-05 23:47:39 +0530

    MOBI-GADGETS

---
+++ mobi-laptop.js
@@ -0,0 +1,1 @@
+Add to Cart → alert
---
+++ mobi-phone.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".product-card button").forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("Mobile skin added to cart!");
+  });
+});
