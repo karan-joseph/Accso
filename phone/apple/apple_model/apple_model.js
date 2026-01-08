8e09150863ced72ee11d651efd086d25a41663a5
karan <karanjarmy@gmail.com>
2026-01-08 21:01:00 +0530

    added apple models

---
+++ phone/apple/apple_model/apple_model.js
@@ -0,0 +1,15 @@
+// selecting side_navbar
+var sidenavbar = document.querySelector(".side_navbar");
+
+function shownavbar() {
+    sidenavbar.style.left = "0";
+}
+
+function closenavbar() {
+    sidenavbar.style.left = "-50%";
+}
+
+// selecting main image
+function changeImage(img) {
+  document.getElementById("mainPhone").src = img.src;
+}
