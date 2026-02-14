550016f4aa52afdda918b32a2cdf24d4c9613d95
karan <karanjarmy@gmail.com>
2026-02-14 21:59:30 +0530

    search

--- phone/apple/apple_models/apple_model.js
+++ phone/apple/apple_models/apple_model.js
@@ -13,3 +13,23 @@ function shownavbar() {
 function closenavbar() {
     sidenavbar.style.left = "-50%";
 }
+var searchInput = document.getElementById("searchInput");
+var modelList = document.querySelectorAll(".models_name div");
+
+searchInput.addEventListener("keyup", function () {
+
+    var enteredValue = searchInput.value.toUpperCase();
+
+    modelList.forEach(function(item) {
+
+        var modelName = item.textContent;
+
+        if(modelName.toUpperCase().indexOf(enteredValue) < 0) {
+            item.style.display = "none";
+        } else {
+            item.style.display = "block";
+        }
+
+    });
+
+});
